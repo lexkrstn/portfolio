c28c2b8527c402a6f43fd16fe8e6c902b2b647ac
Alexander Korostin <l3xkrstn@gmail.com>
2026-02-06 18:37:57 +0100

    fix: ecosystem config

--- api/src/config.ts
+++ api/src/config.ts
@@ -1,3 +1,7 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const TEST = process.env.NODE_ENV === 'test';
 
 function getDatabaseUri() {
--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,31 +1,33 @@
 module.exports = {
-    apps: [
-      {
-        name: "portfolio-api-prod",
-        script: './dist/api.js',
-        cwd: ".",
-        instances: 1,
-        exec_mode: "cluster",
-        max_memory_restart: "200M",
-        env: {
-          API_PORT: 3201,
-          API_HOST: "127.0.0.1",
-          NODE_ENV: "production",
-        },
+  apps: [
+    {
+      name: "portfolio-api-prod",
+      script: './dist/api.js',
+      cwd: ".",
+      instances: 1,
+      exec_mode: "cluster",
+      max_memory_restart: "200M",
+      env: {
+        API_PORT: 3301,
+        API_HOST: "127.0.0.1",
+        NODE_ENV: "production",
       },
-      {
-        name: "portfolio-ssr-prod",
-        script: './dist/ssr.js',
-        cwd: ".",
-        instances: 1,
-        exec_mode: "cluster",
-        max_memory_restart: "200M",
-        env: {
-          API_INTERNAL_URL: "http://127.0.0.1:3201",
-          API_EXTERNAL_URL: "https://akorostin.com",
-          STATIC_URL: "https://static.akorostin.com",
-          NODE_ENV: "production",
-        },
+    },
+    {
+      name: "portfolio-ssr-prod",
+      script: './dist/ssr.js',
+      cwd: ".",
+      instances: 1,
+      exec_mode: "cluster",
+      max_memory_restart: "400M",
+      env: {
+        SSR_HOST: "127.0.0.1",
+        SSR_PORT: 3302,
+        API_INTERNAL_URL: "http://127.0.0.1:3301",
+        API_EXTERNAL_URL: "https://akorostin.com",
+        STATIC_URL: "https://static.akorostin.com",
+        NODE_ENV: "production",
       },
-    ],
-  };
+    },
+  ],
+};
